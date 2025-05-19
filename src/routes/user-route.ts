14ae3a2718484fe592c3145655cf4ea1e70213d2
DuyNguyen <duyyb257946@gmail.com>
2025-05-19 17:48:27 +0700

    comment user router

--- src/routes/user-route.ts
+++ src/routes/user-route.ts
@@ -3,5 +3,5 @@ import UserController from "../controller/user";
 
 export const UserRoute = Router();
 
-UserRoute.get("/:id", UserController.fetch);
-UserRoute.put("/:id", UserController.update);
+// UserRoute.get("/:id", UserController.fetch);
+// UserRoute.put("/:id", UserController.update);
